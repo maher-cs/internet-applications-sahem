c5e0b4994910b42994013e6bea0e869dc96f7c2b
maher <maher.azkoul@gmail.com>
2019-10-21 20:57:51 +0300

    front: main-navigation: rotate burger icon on open side nav

--- app/src/app/app-layout/main-nav/main-nav.component.ts
+++ app/src/app/app-layout/main-nav/main-nav.component.ts
@@ -10,6 +10,8 @@ import { map, shareReplay } from 'rxjs/operators';
 })
 export class MainNavComponent {
 
+  open = false;
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
