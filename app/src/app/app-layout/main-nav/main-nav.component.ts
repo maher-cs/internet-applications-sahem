00eff17a493135126e01adfdc4651ccc9cb68e80
maher <maher.azkoul@gmail.com>
2019-10-25 14:17:31 +0300

    front: nav-bar: change background on change url

--- app/src/app/app-layout/main-nav/main-nav.component.ts
+++ app/src/app/app-layout/main-nav/main-nav.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.scss']
 })
-export class MainNavComponent {
+export class MainNavComponent implements OnInit {
+
+  isHomePage: boolean;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -18,6 +21,18 @@ export class MainNavComponent {
 
   constructor(
     private breakpointObserver: BreakpointObserver,
+    public  location: Location
   ) {}
+
+  ngOnInit() {
+    this.checkHomePage();
+  }
+
+  checkHomePage() {
+    this.isHomePage = this.location.path() === '/home';
+    this.location.onUrlChange(
+      x => this.isHomePage = x === '/home'
+    );
+  }
 
 }
